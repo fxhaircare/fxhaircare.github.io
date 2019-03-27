c67ae1b9ad8efc62a3492bdbf2bd43dc3526eb39
Nathan Chen <nchen5@cs.washington.edu>
2019-03-26 21:04:40 -0700

    redid front and about pages

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -6,15 +6,53 @@ const AboutPage = () => {
   return (
     <Container>
       <Segment vertical>
-        <Header as="h2">
+        {/* <Header as="h2">
           <Header.Content>
-            About
+            f(x) beauty
           </Header.Content>
-        </Header>
+        </Header> */}
       </Segment>
+    
+      <img src="/static/Succulents_Light_1_600x.jpg" ></img>
+      <img src="/static/Andrea_Hair_600x.jpg"></img>
+      <img src="/static/Succulents_Dark_600x.jpg"></img>
+
       <Segment vertical>
         <p>
-          This starter was created by @fabien0102.
+          <i>"Natural inputs. Real outputs. Built to function for you."</i>
+        </p>
+        <p>
+          F(X) BEAUTY is the newest project coming from UW Alpha Kappa Psi's Innovate Committee. Backed by 8 students 
+          in fields of study ranging from
+          Public Health and Marketing to Computer Science and Design, f(x) is dedicated to creating a haircare 
+          experience that works for everyone, no matter their hair type, gender, or profession.
+        </p>
+        <p>
+          That's why, since the summer of 2018, our team has spent weeks developing a line of hair oils with three key
+          principles in mind: affordability, sustainability, and usability. 
+        </p>
+        <p>
+          <strong>Affordability</strong>: As students, we understand the need for high quality products priced at an 
+          affordable cost. 
+          Available to you at price points much lower than that of our competitors', f(x) products nourish and 
+          revitalize your hair without breaking the bank. 
+        </p>
+        <p>
+          <strong>Sustainability</strong>: To us, affordable doesn't mean artificial. All f(x) hair oils are made with
+           natural ingredients. Blending
+          together a combination of fresh scents, earth-friendly oils, and natural preservatives, it's no wonder our
+           beauty essentials boast both
+          the shortest list of ingredients and the highest impact on the market. 
+        </p>
+        <p>
+          <strong>Usability</strong>: f(x) products are made for everyone, regardless of identity. Our hair oils are
+           compatible with
+          any and every hair type (beards included!) and delivered to you in convenient dropper bottles
+          perfect for use at home or on-the-go. 
+        </p>
+        <p>
+          As a relatively new brand, f(x) is just getting started. Thank you for your support, and we hope you enjoy
+           our line of products as much as we enjoyed making them.
         </p>
       </Segment>
     </Container>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ import {
   Header,
   Icon,
 } from "semantic-ui-react";
-// import Checkout from "../components/checkout"
+import Carousel from "nuka-carousel";
 
 const IndexPage = (props: LayoutProps) =>
   <div>
@@ -23,17 +23,36 @@ const IndexPage = (props: LayoutProps) =>
         <Header inverted as="h1">f(x) beauty</Header>
         <Button primary size="huge">Shop Now</Button>
       </Container>
+      <Segment>
+      <Carousel wrapAround={true} autoplay={true} heightMode={null} initialSlideHeight={600}>
+      <img src="/static/banner/img4.jpg" ></img>
+      <img src="/static/banner/img5.jpg" ></img>
+      <img src="/static/banner/img1.jpg" ></img>
+      <img src="/static/banner/img2.jpg" ></img>
+      <img src="/static/banner/img3.jpg" ></img>
+      <img src="/static/banner/img6.jpg" ></img>
+      </Carousel>
+      </Segment>
+
     </Segment>
-    {/* <Checkout/> */}
-    {/* About this starter */}
+
     <Segment vertical className="stripe">
       <Grid stackable verticalAlign="middle" className="container">
         <Grid.Row>
           <Grid.Column width="8">
             <Header>Function with Us</Header>
             <p>
-              Our line of beauty products is made 
-                </p>
+              Our line of beauty products includes:
+              <ul>
+                <li>f(1) Hair Oil - a soothing mixture of vanilla and lavender</li>
+                <li>f(2) Hair Oil - a subtle marriage of vanilla and cedarwood</li>
+              </ul>
+            </p>
+            <p>
+              With more scents on the way and seasonal favorites available at a moments notice, f(x) 
+              is sure to have the perfect product for you. And if your favorite scent is missing, let us know: 
+              we'd be happy to calculate that personalized formula just for you.
+            </p>
           </Grid.Column>
           <Grid.Column width="6" floated="right">
             {/* TODO replace with a pretty GIF */}
